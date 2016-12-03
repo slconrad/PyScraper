96ec5ba7fddb1edd1b197de16631f91c76fddd09
Shawn Conrad <digitalpic@me.com>
2016-12-03 13:29:59 -0600

    updated forecast to include 'tonight'.

--- forecast.py
+++ forecast.py
@@ -1,57 +1,50 @@
-""" Web scraper utilizing BeautifulSoup and Pandas """
+""" Web scraper for polling weather forecasts utilizing 
+BeautifulSoup and Pandas """
 
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
 
-
+# Setup and retrieve
 page = requests.get(
     "http://forecast.weather.gov/MapClick.php?lat=33.1276&lon=-96.8737")
 soup = BeautifulSoup(page.content, 'html.parser')
+
+# Current temp and weather 
+current = soup.find(id="current_conditions-summary")
+current_weather = current.find(class_="myforecast-current").get_text(" ")
+current_temp = current.find(class_="myforecast-current-lrg").get_text(" ")
+
+# Extended forecast
 seven_day = soup.find(id="seven-day-forecast")
 forecast_items = seven_day.find_all(class_="tombstone-container")
 tonight = forecast_items[1]
 
 period = tonight.find(class_="period-name").get_text(" ")
 short_desc = tonight.find(class_="short-desc").get_text(" ")
-temp = tonight.find(class_="temp").get_text()
 
+# Tonight temp
+tonight_temp = tonight.find(class_="temp-low").get_text()
+
+# Currenlty unused
 img = tonight.find("img")
 desc = img['title']
 period_tags = seven_day.select(".tombstone-container .period-name")
 periods = [pt.get_text() for pt in period_tags]
-periods
 short_descs = [sd.get_text()
                for sd in seven_day.select(".tombstone-container .short-desc")]
 temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
-descs = [d["title"] for d in seven_day.select(".tombstone-container img\n")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
 
-# ['Tonight',
-#  'Thursday',
-#  'ThursdayNight',
-#  'Friday',
-#  'FridayNight',
-#  'Saturday',
-#  'SaturdayNight',
-#  'Sunday',
-#  'SundayNight']
-
-weather = pd.DataFrame({
-    "period": periods,
-    "short_desc": short_desc,
-    "temp": temps,
-    "desc": descs
-})
-
-weather
-
-print(period)
-print(temp)
-print(short_desc)
-print("=============")
+# Print to conole
+print('\n')
+print('CURRENT WEATHER IN DFW')
+print(current_weather)
+print(' ' + current_temp)
+print(' Tonight '+ tonight_temp + ', ' + short_desc)
+print("Extende Forecast")
 print(descs)
 # print('=============')
 # print(short_descs)
 # print(temps)
-# print(descs)
+# print(periods)
 # print('=============')
