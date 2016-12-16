c110991b90ae2e4d719cca2e2ec84840b6d0e672
Shawn Conrad <digitalpic@me.com>
2016-12-15 21:32:08 -0600

    updates

--- sports_stats.py
+++ sports_stats.py
@@ -13,9 +13,10 @@ team_stats = soup.find(class_='sub-title').get_text()
 team_leaders = soup.find(class_='mod-container mod-stat-leaders')
 
 # Find the players
-players = [p.get_text() for p in soup.select(".mod-container .mod-stat")]
+players = [p.get_text() for p in soup.select(".player-info")]
 
 # Write to CSV
+# TODO 'http://stackoverflow.com/questions/4607920/python-strip-html-from-text-data'
 with open('sports_stats.csv', 'a', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow([team_name, team_stats, team_leaders])
