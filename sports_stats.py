5afce4668b271f13e0dc19b64c39342d5138d5d4
Shawn Conrad <Shawn Conrad>
2016-12-16 15:57:05 -0600

    NA

--- sports_stats.py
+++ sports_stats.py
@@ -1,8 +1,9 @@
-''' Scraper for pulling stats from ESPN'''
+''' Scraper for pulling stats from ESPN and logging to CSV'''
 
 from bs4 import BeautifulSoup
 import requests
 import csv
+
 
 page = requests.get('http://www.espn.com/nfl/team/stats/_/name/dal')
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -10,20 +11,19 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # Find the team
 team_name = soup.find(class_='logo').get_text()
 team_stats = soup.find(class_='sub-title').get_text()
-team_leaders = soup.find(class_='mod-container mod-stat-leaders')
 
 # Find the players
-players = [p.get_text() for p in soup.select(".player-info")]
+team_leaders = [p.get_text(" ", strip=True)
+                for p in soup.select(".mod-container .mod-stat")]
 
 # Write to CSV
-# TODO 'http://stackoverflow.com/questions/4607920/python-strip-html-from-text-data'
 with open('sports_stats.csv', 'a', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow([team_name, team_stats, team_leaders])
 
-
 print('\n')
 print(team_name + ', ' + team_stats)
 print('====================')
-print(players)
+print(team_leaders)
+print('====================')
 print('Written to file.')
